b50ef52e668eddf10e64a3d2543725cba10a206e
sosin <svstar94@gmail.com>
2025-01-17 11:54:37 +0900

    feat: update app

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import streamlit as st
 
-import solve
-
-text = solve.my_func()
-
 st.title('Deploy App')
 st.write('My First Deploy App !')
-st.write(text)
+
+
+import os
+key = os.environ.get('MY_SECRET', 'NOT SET YET')
+st.write(f'Server Key : {key}')
